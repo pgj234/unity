99ea5bb8253b70ff365896c5528afdd0fb741c46
pgj234 <pgj94@naver.com>
2020-08-31 17:33:12 +0900

    commit

---
+++ HelloUnity/Assets/Scripts/Human.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour {
+    public string human_Name;
+
+    public char bloodType;
+
+    public uint age;
+
+    public float height;
+
+    public bool isFemale;
+
+    public void About_Me() {
+        Debug.Log("내 이름은 " + human_Name);
+        Debug.Log("내 혈액형은 " + bloodType);
+        Debug.Log("내 나이는 " + age);
+        Debug.Log("내 키는 " + height);
+        Debug.Log("내가 여자야? " + isFemale);
+    }
+}
---
+++ HelloUnity/Assets/Scripts/Human_Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human_Manager : MonoBehaviour {
+    float a = 1.0f;
+    public Player player;
+    public Human[] humans;
+
+    // public Human human_1;
+    // public Human human_2;
+    // public Human human_3;
+
+    void Start() {
+        // Debug.Log("-------첫번째 휴먼--------");
+        // humans[0].About_Me();
+        // Debug.Log("-------두번째 휴먼--------");
+        // humans[1].About_Me();
+        // Debug.Log("-------세번째 휴먼--------");
+        // humans[2].About_Me();
+
+        // for (int i=0; i<humans.Length; i++) {
+        //     Debug.Log("-------------------------------------------------");
+        //     humans[i].About_Me();
+        // }
+
+        foreach (int f in player.favorite) {
+            Ending(f);
+        }
+
+        for (int i=0; i<player.favorite.Length; i++) {
+            Ending(player.favorite[i]);
+        }
+
+        int j = 0;
+        while (j < player.favorite.Length) {
+            Ending(player.favorite[j]);
+            j += 1;
+        }
+    }
+
+    void Ending(float fav) {
+        if (fav >= 100) {
+            Debug.Log("히든 엔딩");
+        }
+        else if (IsGoodEnding(fav)) {
+            Debug.Log("굿 엔딩");
+        }
+        else {
+            Debug.Log("배드 엔딩");
+        }
+    }
+
+    bool IsGoodEnding(float f) {
+        //return f >= 50;
+        return (f > 50) && (f < 100);
+    }
+}
