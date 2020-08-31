5509b02244fa3fad4ef9acac5ab89b0968e1d84d
pgj234 <pgj94@naver.com>
2020-08-31 12:13:45 +0900

    first commit

---
+++ HelloUnity/Assets/Scripts/Hello_Unity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_Unity : MonoBehaviour {
+    void Start() {
+        Debug.Log("Hello Unity!");
+
+        Debug.LogError("Error!!!");
+    }
+
+}
